381d0771ec79fbfa169aa1c1f40eec2412ca6889
Mateusjose <mateus.s.o.jose@gmail.com>
2019-03-05 20:24:30 +0100

    ready for production 1.2

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,6 @@ import DS from 'ember-data';
 
 
 export default DS.JSONAPIAdapter.extend({
-    // host:'https://agile-shore-96727.herokuapp.com/api'
     host:'https://agile-shore-96727.herokuapp.com/api/v1/requests'
     //namespace:'api',
 });
